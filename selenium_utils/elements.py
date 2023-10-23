3fc0e721122fe1400b413604495047e6ab5031cb
Tokár Dániel <tokardaniel5000@gmail.com>
2023-10-24 01:13:25 +0200

    felesleges fájlok törlése

--- selenium_utils/elements.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class Elements:
-
-    @classmethod
-    def find_element_by_xpath(cls, driver: object, xpath: str) -> object:
-        return driver.find_element(By.XPATH, xpath)
