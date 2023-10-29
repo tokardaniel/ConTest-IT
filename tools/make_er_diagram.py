c6b9cfb400131ddb7dd1163bccbddae74f15af34
Tokár Dániel <tokardaniel5000@gmail.com>
2023-10-29 22:10:58 +0100

    er diagran generálás

---
+++ tools/make_er_diagram.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from dotenv import load_dotenv
+from eralchemy2 import render_er
+
+sys.path.insert(0, os.getcwd())
+
+load_dotenv(os.path.join(os.getcwd(), ".env"))
+
+from database.migration import Migration
+
+
+class MakeErDiagram:
+
+    def __init__(self) -> None:
+        migration = Migration()
+        migration.run()
+        self.engine = migration.engine
+
+    def generate_er_diagram(self):
+        filename = 'mymodel.png'
+
+        render_er(self.engine.url, filename)
+
+
+if __name__ == '__main__':
+    makeErDiagram = MakeErDiagram()
+    makeErDiagram.generate_er_diagram()
