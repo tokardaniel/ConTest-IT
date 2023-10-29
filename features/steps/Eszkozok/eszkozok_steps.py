0d26b85d7226a359df768a7048b617b77462d8d2
Tokár Dániel <tokardaniel5000@gmail.com>
2023-10-29 14:04:19 +0100

    fix

--- features/steps/Eszkozok/eszkozok_steps.py
+++ features/steps/Eszkozok/eszkozok_steps.py
@@ -31,7 +31,7 @@ def step_impl(c):
                     Eszkozok.insert_device_name_to_input(c, f"{device.manufacturer} {device.model}")
                     Eszkozok.select_partner_from_combobox(c, name=f"{partner.first_name} {partner.last_name}")
                     time.sleep(2)
-                    Eszkozok.select_site_from_combobox(c, name=f"{replaced_zip_code} {site.city}, {site.street_name} {site.house_number}")
+                    Eszkozok.select_site_from_combobox(c, name=site.street_name)
                     Eszkozok.insert_description_to_input(c, ds=device.platform)
                     Eszkozok.insert_comment_to_input(c, comm=device.serial_number)
                     Eszkozok.save(c)
